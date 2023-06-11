bcf77adccb93ee37375546988603d9c0cabba5cf
Ivlev Grigoriy <igs94@mail.ru>
2023-06-11 21:03:27 +0300

    feat: add cards route

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', require('./routes/users'));
+app.use('/', require('./routes/cards'));
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
   .then(() => console.log('Connected!'));
