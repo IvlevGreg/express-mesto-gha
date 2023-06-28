57de1c52a7e3d53bdcf4e54801fe1555f5205985
Ivlev Grigoriy <igs94@mail.ru>
2023-06-28 22:07:01 +0300

    refactor: errorHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,29 +1,13 @@
-const getValidationErrorText = (errors) => `${Object.values(errors).map((error) => error.message).join(', ')}`;
-
-const DEFAULT_400_ERROR_TEXT = 'Переданы некорректные данные';
-const DEFAULT_500_ERROR_TEXT = 'Упс... произошла ошибка';
-
 module.exports = (err, req, res, next) => {
-  let { statusCode, message } = err;
-  const { name } = err;
-
-  switch (name) {
-    case 'ValidationError':
-      statusCode = 400;
-      message = getValidationErrorText(err);
-      break;
-    case 'CastError':
-      statusCode = 400;
-      message = DEFAULT_400_ERROR_TEXT;
-      break;
-    default:
-      if (!statusCode) statusCode = 500;
-      if (!message) message = DEFAULT_500_ERROR_TEXT;
-  }
+  const { statusCode = 500, message } = err;
 
   res
     .status(statusCode)
-    .send({ message });
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
 
   next();
 };
