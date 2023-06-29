f363015dbb49e9575a740c7e95bda4e11a6f78fc
Ivlev Grigoriy <igs94@mail.ru>
2023-06-29 20:13:22 +0300

    feat: add index for user schema

--- models/user.js
+++ models/user.js
@@ -2,21 +2,23 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Неправильный формат почты',
+    },
+  },
   name: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: validator.isEmail,
-      message: 'Неправильный формат почты',
-    },
   },
   password: {
     type: String,
