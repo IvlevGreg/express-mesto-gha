c09c3dbe06bed96379c36b6c3f8d2b348e0ba35d
Ivlev Grigoriy <igs94@mail.ru>
2023-07-01 11:23:20 +0300

    feat: add link pattern

--- utils/linkPattern.js
+++ utils/linkPattern.js
@@ -1,4 +1,5 @@
-const LINK_PATTERN = /(https?:\/\/)[(a-z0-9-\._~:\/\?#\[\]@!\$&\'\(\)\*\+\,\;;=)]*/im;
+// eslint-disable-next-line no-useless-escape
+const LINK_PATTERN = /(https?:\/\/)[(a-z0-9-._~:/?#\[\]\$\(\)\*\+@!&',;=]*/im;
 
 module.exports = {
   LINK_PATTERN,
